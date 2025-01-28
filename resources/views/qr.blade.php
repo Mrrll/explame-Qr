6b0bd96180555daa3802b845e9add890ee8f1955
Mrrll <edgarmarrollo@gmail.com>
2025-01-28 18:36:14 +0100

    Agregar vista qr.blade.php para mostrar códigos QR generados

---
+++ resources/views/qr.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QR Code</title>
+</head>
+<body>
+    <h1>Tu código QR</h1>
+    <div>
+        {!! $qrCode !!}
+    </div>
+</body>
+</html>
